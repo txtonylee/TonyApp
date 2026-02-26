0f45ced91f5031e6636872ffe33bb3a308695997
TONYPC <txtonylee@gmail.com>
2026-02-25 23:23:40 -0600

    OnDataItemDeleting, and bulk insert 50k rows of data

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,9 +1,11 @@
+
 BULK INSERT dbo.Product
-FROM 'C:\Users\TONY\Downloads\Compressed\data1.csv'
+FROM 'C:\Users\TONY\Downloads\Compressed\tech_50k.csv'
 WITH (
     DATAFILETYPE = 'char',
     FIELDTERMINATOR = ',',  -- use ',' for CSV files
     ROWTERMINATOR = '\n',   -- use '\n' for new lines
     FIRSTROW = 2,           -- skip header row if one exists
+    BATCHSIZE = 5000,
     TABLOCK                 -- improves performance
 );
--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -3,3 +3,7 @@ select * from data
 delete from data
 
 select * from Product
+
+select * from Product_Staging;
+
+EXEC sp_help 'dbo.Product';
